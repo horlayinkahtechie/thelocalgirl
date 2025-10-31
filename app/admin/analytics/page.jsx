ce17f1858688a2f52b5dc097eb75928dad6f7dd3
horlayinkahtechie <horlayinkah2005@gmail.com>
2025-10-31 19:03:30 +0100

    .

---
+++ app/admin/analytics/page.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Analytics from "./analytics";
+
+export default function page() {
+  return (
+    <div>
+      <Analytics />
+    </div>
+  );
+}
---
+++ app/admin/orders/page.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Orders from "./orders";
+
+export default function page() {
+  return (
+    <div>
+      <Orders />
+    </div>
+  );
+}
